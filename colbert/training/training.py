d158cb6cd64ebe0218b7139b6992d8e05e7a830c
jonghwi-kim <jonghui2@naver.com>
2023-11-21 15:59:40 +0900

    Update colbert/training/training.py

--- colbert/training/training.py
+++ colbert/training/training.py
@@ -97,7 +97,7 @@ def train(args):
                 loss = criterion(scores, labels[:scores.size(0)])
                 loss = loss / args.accumsteps
 
-            if args.rank < 1 and batch_idx % 10 == 0:
+            if args.rank < 1 and batch_idx % 100 == 0:
                 print_progress(scores)
 
             amp.backward(loss)
@@ -120,5 +120,5 @@ def train(args):
             Run.log_metric('train/throughput', num_examples_seen / elapsed, step=batch_idx, log_to_mlflow=log_to_mlflow)
 
             manage_checkpoints(args, colbert, optimizer, batch_idx+1)
-            if batch_idx % 10 == 0:
+            if batch_idx % 100 == 0:
                 print_message(batch_idx+1, avg_loss)
